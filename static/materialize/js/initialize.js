0b8e3c96a13e42487c757e8bf646de546245c089
Karim Luy <karim.luy@he-arc.ch>
2019-03-07 19:00:12 +0100

    setup navbar for mobile

--- static/materialize/js/initialize.js
+++ static/materialize/js/initialize.js
@@ -1,4 +1,11 @@
-//initializing parallax for materialize
+// This document contains various MaterializeCSS JS initialization functions
+
+//initializing parallax for
 $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+// initialize mobile navbar
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
