b32e0e32339588808638c67cd86a7d8cabbf905b
Karim Luy <contact@karimluy.ch>
2019-07-26 14:41:03 +0000

    setup config file for production and ignored it, created a maintenance page

--- launch.sh
+++ launch.sh
@@ -1,2 +1,10 @@
-python3 dl.py 11XnhJaFVZ-rRpSqSYFPkvJ4M9rZF3n8_ ./static.zip
-sudo FLASK_APP=main.py FLASK_ENV=production python3 -m pipenv run flask-h 0.0.0.0 -p 80
+python3 dl.py {{ filename  }} ./static.zip
+# config files:
+# nginx
+/etc/nginx/sites-enabled/159.100.242.211.conf
+
+# uwsgi
+python3 -m pipenv run uwsgi --http-socket :8080 --wsgi-file app.py
+
+# service
+/etc/systemd/system/FlaskRTP.service
