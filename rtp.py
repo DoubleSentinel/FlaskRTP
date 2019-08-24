2d2d30c9758b99d30adf139f794b6c835791bcb2
Karim Luy <karim.luy@he-arc.ch>
2019-08-24 16:47:33 +0200

    added some user request features (partner logos, some french correction)

--- rtp.py
+++ rtp.py
@@ -74,9 +74,9 @@ def boat():
         ("media/images/boat/boat_3.JPG","Bar sur le pont à l'arrière du bateau"),
         ("media/images/boat/boat_4.JPG","Cuisine avec réchaud à gaz, réfrigérateur, et congélateur"),
         ("media/images/boat/boat_5.JPG","Cabine tout confort avec lit double et rangements"),
-        ("media/images/boat/boat_6.JPG","Salle de bain privatif (WC, douche, et lavabo)"),
-        ("media/images/boat/boat_7.JPG","L'énergie électrique est 100% vert grâce aux panneaux solaires"),
-        ("media/images/boat/boat_8.JPG","Quatre membres d'équipage (Chef cuisinière, capitaine, stewards)"),
+        ("media/images/boat/boat_6.JPG","Salle de bain privative (WC, douche, et lavabo)"),
+        ("media/images/boat/boat_7.JPG","L'énergie électrique est 100% verte grâce aux panneaux solaires"),
+        ("media/images/boat/boat_8.JPG","5 membres d'équipage (Capitaine, 2 matelos, cheffe cuisinière, guide)"),
         ]
     return render_template('presentation/boat.html',
             active='presentation', features=features)
